885ee01c17752eb9d45256cca5d505f91ce3977d
Cem Ersoz <cersozz@gmail.com>
2018-05-15 18:01:51 +0300

    Update i2c motor control
    Works for latest motor controller

--- i2c_motorcontroller/ic2_motorcontroller.ino
+++ i2c_motorcontroller/ic2_motorcontroller.ino
@@ -1,25 +1,26 @@
 #include <Wire.h>
 
-#define i2c_id 0x07
+#define i2c_id 0x49
 
-int m1A = 7
-int m1B = 8
-int m1E = 6
+#define op_init 0x01
+#define op_motor 0x02
+#define op_servo 0x03
+#define op_encoder 0x04
+#define m1A 8
+#define m1E 6
 
-int m2A = 10
-int m2B = 9
-int m2E = 11
+#define m2A 7
+#define m2E 5
   
-int m3A = 4
-int m3B = 2
-int m3E = 3
+#define m3A 12
+#define m3E 11
 
-int m4A = 0
-int m4B = 1
-int m4E = 5
-  
+#define m4A 10
+#define m4E 9
+int motors[][3] = {{m1A,m1E}, {m2A,m2E}, {m3A,m3E},{m4A,m4E}};
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+
   Serial.begin(9600);
   Wire.begin(i2c_id);  
   delay(100);
@@ -36,21 +37,34 @@ void loop() {
   delay(1000);
   // put your main code here, to run repeatedly:
 
-}
+}       
 
+void set_motor_speed(int motor_no, boolean motor_dir, int motor_speed){
+  analogWrite(motors[motor_no-1][1],motor_speed);
+  digitalWrite(motors[motor_no-1][0], motor_dir);
+}
 void receive_event(int n){
   Serial.println("received: ");
-  Serial.print(n);
   while(Wire.available()){
-    Serial.println("read: ");
-    Serial.print(Wire.read());
+    switch(Wire.read()){
+      case op_motor:
+        if(!Wire.available()) return;
+        int motor_no = Wire.read();
+        if(!Wire.available()) return;
+        boolean motor_dir = Wire.read();
+        if(!Wire.available()) return;
+        int motor_speed = Wire.read();
+        set_motor_speed(motor_no, motor_dir, motor_speed);
+      break;
+    }
+    Serial.print("read: ");
   }
 }  
   
 void request_event(){
   Serial.println("requested: ");
   while(Wire.available()){
-    Serial.println("read: ");
-    Serial.print(Wire.read());
+    Serial.print("read: ");
+    Serial.println(Wire.read());
   }
 }
